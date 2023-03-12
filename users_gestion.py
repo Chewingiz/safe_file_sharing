3736b7ad29f1b99edc277dd2846fa80f689ea578
Manal <andnothingevrything@gmail.com>
2023-03-12 17:50:38 +0100

    added function to add a user

--- users_gestion.py
+++ users_gestion.py
@@ -15,12 +15,9 @@ S_psw    = py_dict["password"]
 
 
 
-def add_file( file_name):
-    ftp = ftplib.FTP(S_host_n)
-    ftp.login(user = S_user_n, passwd = S_psw)
+def add_file(ftp, file_name):
     with open(file_name, "rb") as file:
         ftp.storbinary("STOR " + file_name, file)
-    ftp.quit() 
 
 #delete file
 def delete_file( file_name):
@@ -32,7 +29,40 @@ def delete_file( file_name):
     ftp.delete(file_name)
     ftp.quit() 
 
+#create new user
 
+def add_new_user( name, public_key):
+    ftp = ftplib.FTP(S_host_n)
+    ftp.login(user = S_user_n, passwd = S_psw)
 
-delete_file("users.json")
+    filename = "users.json"
+
+    if filename in ftp.nlst():
+        print("Le fichier existe sur le serveur FTP")
+        with open(filename, 'rb') as file:
+            lines = file.readlines()
+            # Supprimer la dernière ligne
+            lines.pop()
+            lines.pop()
+            
+            # Ajouter une nouvelle ligne
+            new_line = ("\t},\n\t{\n\t\t\"name\" : \"" + name + "\",\n\t\t\"public_key\" : \"" + public_key + "\"\n\t}\n]}").encode('utf-8')
+            lines.append(new_line)
+
+            # Écrire le fichier mis à jour localement
+            with open(filename, 'wb') as file:
+                file.writelines(lines)
+
+            add_file( ftp, filename)
+
+    else:
+        with open(filename, "w") as new_file:
+            new_file.write("{\n\t\"users\":[{\n\t\t\"name\" : \"" + name + "\",\n\t\t\"public_key\" : \"" + public_key + "\"\n\t}\n]}")
+
+        add_file( ftp, filename)
+
+    ftp.quit()  
+
+add_new_user( "name", "public_key")
+#delete_file("users.json")
 #add_file( "users.json")
