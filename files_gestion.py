ac3830cbbf1382c0c759b7d546e58de035f120eb
Manal <andnothingevrything@gmail.com>
2023-03-13 02:33:49 +0100

    added send file i have to test it

--- files_gestion.py
+++ files_gestion.py
@@ -1,13 +1,28 @@
 import json
 import ftplib
-from rsa import gen_rsa_keypair
+from rsa import rsa_enc, rsa_dec, rsa_sign, rsa_verify, h 
 from Crypto.Cipher import AES
 import os
 
+from users_gestion import get_key
 
-#AES
+with open("server_info.json") as my_file:
+    json_str = my_file.read()
+
+py_dict = json.loads(json_str)
+
+type(py_dict)
+
+# Server informations
+S_host_n = py_dict["host_name"]
+S_user_n = py_dict["username"]
+S_psw    = py_dict["password"]
+
+
 def generate_key():
     return os.urandom(16)
+
+#AES
 
 def encrypt(message, key):
     cipher = AES.new(key, AES.MODE_EAX)
@@ -18,3 +33,43 @@ def decrypt(ciphertext, tag, key):
     cipher = AES.new(key, AES.MODE_EAX, nonce=cipher.nonce)
     plaintext = cipher.decrypt_and_verify(ciphertext, tag)
     return plaintext
+
+def send_to(sender, receptionist, file_name ):
+    ftp = ftplib.FTP(S_host_n)
+    ftp.login(user = S_user_n, passwd = S_psw)
+
+    receptionist_public_key = get_key(receptionist)
+    sender_public_key = get_key(sender)
+    sender_private_key = 0 # get from test_password( local_psw)
+
+    key = generate_key()
+    enc_key = rsa_enc(key, receptionist_public_key)
+    signature = rsa_sign(enc_key, sender_private_key)
+
+    with open(file_name, "r") as file:
+        message = file.read()
+        ciphertext, tag = encrypt(message, key)
+        with open("key_" + file_name, "w") as file2:
+            file2.write("{\n\t\"sender\":" + sender  + "\n\t\"key\":" + enc_key  + "\n\t\"signature\":" + signature + "\n\t\"tag\":" + tag + " }")
+        with open("enc_" + file_name, "w") as file3:
+            file2.write(ciphertext)
+    ftp.cwd("./" + receptionist)
+
+    add_file( ftp, "enc_" + file_name)
+    add_file( ftp, "key_" + file_name)
+
+    ftp.cwd("..")
+    #test #rsa_verify(signature, sender_public_key)==h(m)
+"""
+def get_file(file_name)
+    my_private_key = 0 # get from test_password( local_psw)
+
+
+    with open("key_" + file_name) as my_file:
+        json_key = my_file.read()
+
+    file_info_dict = json.loads(json_key)
+    
+    return user_dict
+
+"""
